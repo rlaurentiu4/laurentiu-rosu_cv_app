3e400a3fbe41627b85929305fcd6cd429ba23890
LaurentiuBadsi <50487193+LaurentiuBadsi@users.noreply.github.com>
2020-07-06 10:50:29 +0300

    website update

--- src/utilities/webData.js
+++ src/utilities/webData.js
@@ -2,6 +2,7 @@ const badsiLogo = require('../assets/images/badsi_logo.png');
 const cariereLogo = require('../assets/images/cariere_badsi.png');
 const serveLogo = require('../assets/images/serve.svg');
 const armsLogo = require('../assets/images/logo_arms_mobile.png');
+const cvLogo = require('../assets/images/cv_logo.png');
 const sharevisionLogo = require('../assets/images/sharevision_log.png');
 
 const webData = [
@@ -9,7 +10,8 @@ const webData = [
   { id: 2, site: 'http://cariere.badsi.ro/', logo: cariereLogo },
   { id: 3, site: 'https://serve.ro/', logo: serveLogo },
   { id: 4, site: 'https://arms.world/', logo: armsLogo },
-  { id: 5, site: 'http://sharevision.ro/en/home/', logo: sharevisionLogo },
+  { id: 5, site: 'https://catalin-voicu.ro/', logo: cvLogo },
+  { id: 6, site: 'http://sharevision.ro/en/home/', logo: sharevisionLogo },
 ];
 
 export default webData;
